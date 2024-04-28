cbb5ebf5a536ecaa8a3679b0ca0e2632425e0f90
RedPinkFox <168366639+RedPinkFox@users.noreply.github.com>
2024-04-28 23:23:55 +0300

    Pohui
    Camera Z fix

--- Player_script.gd
+++ Player_script.gd
@@ -4,6 +4,7 @@ const SPEED = 0.1
 const CAMERA_VERTICAL_SENSIVITY = 0.1
 const CAMERA_HORIZONTAL_SENSIVITY = 0.1
 const ROTATION_SPEED = 0.05
+const STABILIZE_SPEED = 0.005
 
 @export var SpeedLabel: RichTextLabel
 @export var FPS: RichTextLabel
@@ -30,6 +31,15 @@ func _physics_process(delta):
 		velocity += get_basis() * Vector3(0, 0, SPEED)
 	if (Input.is_action_pressed("stop") && (velocity.length() <= 0.5)):
 		velocity = Vector3.ZERO
+	if (Input.is_action_pressed("camera_stabilize")):
+		if get_rotation().x > 0:
+			self.rotate_x(-STABILIZE_SPEED)
+		elif get_rotation().x < 0:
+			self.rotate_x(STABILIZE_SPEED)
+		if get_rotation().z > 0:
+			self.rotate_z(STABILIZE_SPEED)
+		elif get_rotation().z < 0:
+			self.rotate_z(STABILIZE_SPEED)
 	SpeedLabel.text = "Speed: " + str(velocity.length()).substr(0, 4)
 	FPS.text = str(Engine.get_frames_per_second())
 
@@ -41,6 +51,15 @@ func _unhandled_input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
 	if (Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED) and (event is InputEventMouseMotion):
+		print(str(get_rotation()) + 'p')
+		#self.rotate_y(event.relative.x * CAMERA_VERTICAL_SENSIVITY)
+		#self.rotate_z(event.relative.y * CAMERA_HORIZONTAL_SENSIVITY)
+		var past_angle_z = get_basis().get_euler().z
 		rotate_object_local(Vector3(0, 1, 0), deg_to_rad(-event.relative.x * CAMERA_HORIZONTAL_SENSIVITY))
 		rotate_object_local(Vector3(1, 0, 0), deg_to_rad(-event.relative.y * CAMERA_VERTICAL_SENSIVITY))
+		self.set_rotation(Vector3(get_basis().get_euler().x, get_basis().get_euler().y, past_angle_z))
+		#self.rotate_z(rad_to_deg(dif_angle_z))
+		#rotate_object_local(Vector3(0, 0, 1), get_basis().get_euler().z - dif_angle_z)
 		#rotate(transform.basis.x.normalized(), -deg_to_rad(event.relative.y * CAMERA_VERTICAL_SENSIVITY)) # rotate around local axis
+		print(str(get_rotation()) + 'f')
+		print()
