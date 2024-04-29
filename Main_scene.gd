dd4f44a992676080e15230abb718b0d331ef0195
Alexandr <Alexresh62@yandex.ru>
2024-04-29 12:24:49 +0300

    added visual speed control

--- Main_scene.gd
+++ Main_scene.gd
@@ -1,20 +1,23 @@
 extends Node3D
 
-@export var ExitBtn: Button
+@export var MenuContainer: VBoxContainer
 
 func _ready():
-	ExitBtn.visible = false
+	MenuContainer.visible = false
 
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_cancel")):
 		if (Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			ExitBtn.visible = true
+			MenuContainer.visible = true
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			ExitBtn.visible = false
-		
+			MenuContainer.visible = false
+
+func _on_resume_btn_pressed():
+	MenuContainer.visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-func _on_button_pressed():
+func _on_exit_button_pressed():
 	get_tree().quit(0)
